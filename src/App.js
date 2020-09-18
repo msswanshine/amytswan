ae9537eb69d526894643e4f3fee75d0895e643cb
Amy Swan <amyswan@MacBook-Pro.local>
2020-09-17 17:36:28 -0700

    Add header and footer elements.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
+
+import Header from "./components/header";
+import Footer from "./components/footer";
+
 import "./App.css";
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <Header />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "React";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Amy T. Swan</h1>
+      <p>Musician Gone Coder - Frontend Developer</p>
+    </header>
+  )
+}
